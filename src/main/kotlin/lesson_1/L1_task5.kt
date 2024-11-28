a7622c62a9fb1407389edbbfc667f374f02b5001
ToothLonely <arsen.shirinov2004@gmail.com>
2024-11-29 01:14:26 +0300

    OnB-1-5 add new variables and fix formatting

--- src/main/kotlin/lesson_1/L1_task5.kt
+++ src/main/kotlin/lesson_1/L1_task5.kt
@@ -1,8 +1,17 @@
 package lesson_1
 
+const val SECONDS_IN_MINUTE = 60
+const val MINUTES_IN_HOURS = 60
+const val SECONDS_IN_HOURS = 3600
+
 fun main() {
+    //Объявляю переменные
     val fullTimeInSeconds: Int = 6480
-    val minutes: Int = fullTimeInSeconds / 60 % 60
-    val seconds: Int = fullTimeInSeconds % 60
-    println("0${fullTimeInSeconds / 3600}:$minutes:0$seconds")
+    val seconds: Int = fullTimeInSeconds % SECONDS_IN_MINUTE
+    val minutes: Int = fullTimeInSeconds / SECONDS_IN_MINUTE % MINUTES_IN_HOURS
+    val hours: Int = fullTimeInSeconds / SECONDS_IN_HOURS
+
+    //Привожу данные к формату времени
+    val formattedTime = String.format("%02d:%02d:%02d", hours, minutes, seconds)
+    println(formattedTime)
 }
